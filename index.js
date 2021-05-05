e14e527095bf1a773d5167e4cc492a7e208945be
Ravi <ravisinghxxxxl@gmail.com>
2021-05-05 11:31:17 +0300

    Display books in table function added

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
-console.log('start');
+console.log("start");
 
 function Book(name, author, type) {
-    this.name = name;
-    this.author = author;
-    this.type = type;
+  this.name = name;
+  this.author = author;
+  this.type = type;
 }
 
 //DISPLAY CONSTRUCTOR
-function Display() {
-
-}
+function Display() {}
 
 // ADD METHOD TO DISPLAY PROTOTYPE
-
+Display.prototype.add = function (book) {
+  console.log("Adding to UI");
+  let tableBody = document.getElementById("tableBody");
+  let uiString = `<tr>
+                    <td>${book.name}</td>
+                    <td>${book.author}</td>
+                    <td>${book.type}</td>
+                 </tr> `;
+tableBody.innerHTML += uiString;
+};
+Display.prototype.clear = function () {
+  let libraryForm = document.getElementById("libraryForm");
+  libraryForm.reset();
+};
 
 // ADD SUBMIT EVENtLISTENER TO libraryForm
-let libraryForm = document.getElementById('libraryForm')
-libraryForm.addEventListener('submit', libraryFormSubmit);
+let libraryForm = document.getElementById("libraryForm");
+libraryForm.addEventListener("submit", libraryFormSubmit);
 function libraryFormSubmit(e) {
-    e.preventDefault();
-    console.log('You have sumbmitted a library form !');
-    let name = document.getElementById('bookName').value;
-    let author = document.getElementById('author').value;
-    let type;
-    let fiction = document.getElementById('fiction');
-    let programming = document.getElementById('programming');
-    let cooking = document.getElementById('cooking');
-    
-    if(fiction.checked) {
-        type = fiction.value;
-    }
-    if(programming.checked) {
-        type = programming.value;
-    }
-    if(cooking.checked) {
-        type = cooking.value;
-    }
-    let book = new Book(name, author, type);
-    console.log(book);
+  e.preventDefault();
+  console.log("You have sumbmitted a library form !");
+  let name = document.getElementById("bookName").value;
+  let author = document.getElementById("author").value;
+  let type;
+  let fiction = document.getElementById("fiction");
+  let programming = document.getElementById("programming");
+  let cooking = document.getElementById("cooking");
+
+  if (fiction.checked) {
+    type = fiction.value;
+  }
+  if (programming.checked) {
+    type = programming.value;
+  }
+  if (cooking.checked) {
+    type = cooking.value;
+  }
+  let book = new Book(name, author, type);
+  console.log(book);
+
+  let display = new Display();
+  display.add(book);
+  display.clear();
 }
